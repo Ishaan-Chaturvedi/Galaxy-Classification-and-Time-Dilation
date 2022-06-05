9cf77c17b487c59148324726673663dee901e3f8
archit-2002 <105555740+archit-2002@users.noreply.github.com>
2022-06-06 01:18:52 +0530

    Update Time_Dilation.py

--- Time_Dilation.py
+++ Time_Dilation.py
@@ -11,6 +11,10 @@ for i in range(Q):
     sr = (1-(G * M)/(r * c ** 2 )) ** (0.5)
     gr = (1- (2* G * M)/(r * c ** 2)) ** (0.5)
     t = ( 86400/((sr) + (gr) - 1) ) * ( (sr) + (gr) - (1-((460/c)**2))** (0.5)- (1 - ((2*G*M)/(R*c**(2))))**(0.5))
-    print("{:e}".format(round(t, 5)) + " seconds")
-    
+    if round(t, 5) > 0 :
+        print("Time on satellite is " + "{:e}".format(round(t, 5)) + " seconds per earth day ahead of time on earth's surface")
+    elif  round(t, 5) < 0 :
+        print("Time on satellite is " + "{:e}".format(round(t, 5)) + " seconds per earth day behind of time on earth's surface")
+    else :
+        print("Time on satellite is same as time on earth's surface")
 
